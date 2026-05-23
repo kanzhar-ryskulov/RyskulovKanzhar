f39c4dd2058f3eb5c85f82935be05e2254f19a5f
Kanzhar <ryskulovkanzar@gmail.com>
2026-05-23 16:24:40 +0600

    Made migrations and added project to source\settings.py

--- source/to_do/models.py
+++ source/to_do/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        NEW = 'NEW', 'Новая'
+        IN_PROGRESS = 'IN_PROGRESS', 'В прогрессе'
+        DONE = 'DONE', 'Сделано'
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(
+        max_length=11,
+        choices=Status.choices,
+        default=Status.NEW,
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
