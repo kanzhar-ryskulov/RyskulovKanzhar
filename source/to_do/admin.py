c6af24c55c3e0e87df39290807b2c2d296b76f49
Kanzhar <ryskulovkanzar@gmail.com>
2026-05-23 16:35:59 +0600

    Registered models in admin.py

--- source/to_do/admin.py
+++ source/to_do/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from to_do.models import Task
+
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'date')
+    list_filter = ('status',)
+    ordering = ('status',)
