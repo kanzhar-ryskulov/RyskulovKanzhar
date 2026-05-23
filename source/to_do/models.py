898aa570be18b8e787434e8f726c79657bba1406
Kanzhar <ryskulovkanzar@gmail.com>
2026-05-23 20:02:38 +0600

    Made main page which shows all tasks and templates folder

--- source/to_do/models.py
+++ source/to_do/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Task(models.Model):
     class Status(models.TextChoices):
         NEW = 'NEW', 'Новая'
--- source/to_do/views.py
+++ source/to_do/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from to_do.models import Task
+
+
 # Create your views here.
+def main_page(request):
+    task = Task.objects.all()
+    context = {'task': task}
+    return render(request, 'main_page.html', context)
