540a8ee6b58311032c404770352371425815a594
Kanzhar <ryskulovkanzar@gmail.com>
2026-06-04 14:42:20 +0600

    Added button to delete tasks with confirmation

--- source/to_do/views.py
+++ source/to_do/views.py
@@ -1,56 +1,56 @@
-from django.shortcuts import render, HttpResponseRedirect, Http404, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, Http404, get_object_or_404, redirect
 
 from to_do.models import Task
+from to_do.forms import TaskForm
 
 
-# Create your views here.
 def main_page(request):
     task = Task.objects.all()
     context = {'task': task}
     return render(request, 'task/main_page.html', context)
 
+
+
 def add_task(request):
+    form = TaskForm()
+
     if request.method == 'GET':
-        return render(request, 'task/add_task.html')
+        return render(request, 'task/add_task.html', {'form': form})
     elif request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        status = request.POST['status']
-        detail_description = request.POST['detail_description']
-        date = request.POST['date']
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main_page')
 
-        if date:
-            Task.objects.create(
-                title=title,
-                description=description,
-                status=status,
-                detail_description=detail_description,
-                date=date,
-            )
-        else:
-            Task.objects.create(
-                title=title,
-                description=description,
-                status=status,
-                detail_description=detail_description,
-            )
-
-        return HttpResponseRedirect('/')
+        return render(request, 'task/add_task.html', {'form': form})
     return None
 
 def update_task(request, pk, *args, **kwargs):
+
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'GET':
-        context = {'task': task}
-        return render(request, 'task/update_task.html', context)
+        form = TaskForm(instance=task)
+        return render(request, 'task/update_task.html', {'form': form, 'task': task})
     elif request.method == 'POST':
-        task.title = request.POST['title']
-        task.description = request.POST['description']
-        task.detail_description = request.POST['detail_description']
-        task.status = request.POST['status']
-        task.date = request.POST['date']
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/')
+        return render(request, 'task/update_task.html', {'form': form, 'task': task})
+
+
+
+
+        # task.title = request.POST['title']
+        # task.description = request.POST['description']
+        # task.detail_description = request.POST['detail_description']
+        # task.status = request.POST['status']
+        # task.date = request.POST['date'] or None
+
         task.save()
-    return None
+    return HttpResponseRedirect('/')
 
 
 def task(request, *args, pk, **kwargs)  :
@@ -63,3 +63,10 @@ def detail_task(request, *args, pk, **kwargs):
     context = {'task': tasks}
     return render(request, 'task/detail_description.html', context)
 
+def delete_task(request, *args, pk, **kwargs):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "GET":
+        return render(request, "task/delete_task.html", {'task': task})
+    elif request.method == "POST":
+        task.delete()
+    return HttpResponseRedirect('/')
